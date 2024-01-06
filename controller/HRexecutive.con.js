d11026211d079cba5cc4fb3ab4750d69d31a8914
sahil joya <113753584+sahiljoya@users.noreply.github.com>
2024-01-06 16:21:10 +0530

    change task model

--- controller/HRexecutive.con.js
+++ controller/HRexecutive.con.js
@@ -13,8 +13,8 @@ export const createHR = async (req, res) => {
 };
 export const getHR = async (req, res) => {
     try {
-        const get = await applyjob.findOne({job_id:req.params.id})
-        if (get) {
+        const get = await applyjob.find({job_id:req.params.id})
+        if (get.length > 0) {
             res.status(200).send({ status: true, msg: "get data succesfully", data: get });
         } else {
             res.status(404).send({ status: false, msg: "id not found", data: {} });
