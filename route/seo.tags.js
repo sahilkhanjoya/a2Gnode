b030c9ab749c28d9be92d8c8659e8161e086ecfb
sahil joya <113753584+sahiljoya@users.noreply.github.com>
2024-01-10 19:00:27 +0530

    seo tags

--- route/seo.tags.js
+++ route/seo.tags.js
@@ -3,5 +3,5 @@ import seotag from '../controller/seo.tags.js';
 const tagRouter = express()
 
 tagRouter.post('/seo/tag', seotag.seoTagCreate)
-tagRouter.get('/seo/gats', seotag.seoTagCreate)
+tagRouter.get('/seo/gats/:page', seotag.seoTagCreate)
 export default tagRouter
