af0d0c83a8f732a255cc2cbc9b641b2baeece42e
sahil joya <113753584+sahiljoya@users.noreply.github.com>
2024-01-12 10:32:06 +0530

    seo delete update find all

--- controller/seo.tags.js
+++ controller/seo.tags.js
@@ -13,7 +13,7 @@ class seo {
         }).catch(error => res.status(400).send({ status: false, message: error.message, error: error }))
     }
     async seoGet(req, res) {
-        SeoTag.findOne({ jobId: req.params.id }).then((data) => {
+        SeoTag.findById({ _id: req.params.id }).then((data) => {
             if (!data) return res.status(404).send({ status: false, message: "data not found", data: null })
             res.send({ status: true, message: 'tag gets', data: data })
         }).catch(error => res.status(400).send({ status: false, message: error.message, data: error }))
